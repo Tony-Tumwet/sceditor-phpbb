e64e53c20fa59b67d7db6359bd9a89af546f9128
Tekin Birduezen <t.birduezen@web-coding.eu>
2015-06-22 12:21:26 +0200

    No comma after the last emoticon
    Sizes work correct now #7
    
    Optimized the js a bit

--- nassim/sceditor/styles/all/template/js/sce.js
+++ nassim/sceditor/styles/all/template/js/sce.js
@@ -1,49 +1,58 @@
 /***********************************************************
  * Update size tag to use xx-small-xx-large instead of 1-7 *
  ***********************************************************/
+var sizes = ['25', '50', '75', '100', '150', '175', '200'];
 $.sceditor.plugins.bbcode.bbcode.set('size', {
-	format: function ($elm, content) {
+	format: function ($elem, content) {
 		var fontSize,
-			sizes = ['25', '50', '75', '100', '150', '175', '200'],
-			size = $elm.data('scefontsize');
+			sizesIdx = 0,
+			size = $elem.data('scefontsize');
 
 		if (!size) {
-			fontSize = $elm.css('fontSize');
+			fontSize = $elem.css('fontSize');
 
 			// Most browsers return px value but IE returns 1-7
 			if (fontSize.indexOf('px') > -1) {
 				// convert size to an int
-				fontSize = fontSize.replace('px', '') - 0;
-				size = 1;
+				fontSize = ~~(fontSize.replace('px', ''));
 
-				if (fontSize > 9)
-					size = 2;
-				if (fontSize > 12)
-					size = 3;
-				if (fontSize > 15)
-					size = 4;
-				if (fontSize > 17)
-					size = 5;
-				if (fontSize > 23)
-					size = 6;
-				if (fontSize > 31)
-					size = 7;
+				if (fontSize > 31) {
+					sizesIdx = 6;
+				}
+				else if (fontSize > 23) {
+					sizesIdx = 5;
+				}
+				else if (fontSize > 17) {
+					sizesIdx = 4;
+				}
+				else if (fontSize > 15) {
+					sizesIdx = 3;
+				}
+				else if (fontSize > 12) {
+					sizesIdx = 2;
+				}
+				else if (fontSize > 9) {
+					sizesIdx = 1;
+				}
 			}
-			else
-				size = (~~fontSize) + 1;
+			else {
+				sizesIdx = ~~fontSize;
+			}
 
-			if (size > 7)
-				size = 7;
-			if (size < 1)
-				size = 1;
+			if (sizesIdx > 6) {
+				sizesIdx = 6;
+			}
+			else if (sizesIdx < 0) {
+				sizesIdx = 0;
+			}
 
-			size = sizes[size - 1];
+			size = sizes[sizesIdx];
 		}
 
 		return '[size=' + size + ']' + content + '[/size]';
 	},
 	html: function (token, attrs, content) {
-		return '<span data-scefontsize="' + attrs.defaultattr + '" style="font-size:' + attrs.defaultattr + '">' + content + '</span>';
+		return '<span data-scefontsize="' + attrs.defaultattr + '" style="font-size:' + attrs.defaultattr + '%">' + content + '</span>';
 	}
 });
 
@@ -54,25 +63,26 @@ $.sceditor.command.set('size', {
 				callback($(this).data('size'));
 				editor.closeDropDown(true);
 				e.preventDefault();
-			};
+			},
+			size;
 
-		for (var i = 1; i < 7; i++)
+		for (var i = 1; i < 7; i++) {
 			content.append($('<a class="sceditor-fontsize-option" data-size="' + i + '" href="#"><font size="' + i + '">' + i + '</font></a>').click(clickFunc));
+		}
 
 		editor.createDropDown(caller, 'fontsize-picker', content);
 	},
 	txtExec: function (caller) {
-		var editor = this,
-			sizes = ['xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large'];
+		var editor = this;
 
 		$.sceditor.command.get('size')._dropDown(
 			editor,
 			caller,
-			function (size) {
-				size = (~~size);
-				size = (size > 7) ? 7 : ( (size < 1) ? 1 : size );
+			function (sizesIdx) {
+				sizesIdx = ~~sizesIdx;
+				sizesIdx = (sizesIdx > 6) ? 6 : ( (sizesIdx < 0) ? 0 : sizesIdx );
 
-				editor.insertText('[size=' + sizes[size] + ']', '[/size]');
+				editor.insertText('[size=' + sizes[sizesIdx] + ']', '[/size]');
 			}
 		);
 	}
@@ -90,6 +100,7 @@ $(function () {
 	// Hide the normal BBCode Buttons
 	$('#format-buttons').hide();
 	$('#smiley-box a img').each(function () {
+
 		$(this).click(function () {
 			setSmilie($(this).attr('alt'));
 			return false;
